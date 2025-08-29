96e153047d60afb74234b071870e775188392328
prashant patel <prashantpatel487330@gmail.com>
2025-08-29 18:02:01 +0530

    second folder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useSelector,useDispatch } from "react-redux";
+import { increment,decrement } from "./counterSlice";
+const App=()=>{
+    const mycount=useSelector(state=>state.mycounter.count);
+    const dispatch=useDispatch();
+    return(
+        <>
+        <h1>Welcome to my App</h1>
+        <button onClick={()=>{dispatch(increment())}}>Increment</button>
+        <h1>Count:{mycount}</h1>
+        <button onClick={()=>{dispatch(decrement())}}>Decrement</button>
+        </>
+    )
+}
+export default App;
---
+++ src/store.jsx
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import myReducer from "./counterSlice";
+const store =configureStore({
+    reducer:{
+        mycounter:myReducer
+    }
+})
+export default store;
