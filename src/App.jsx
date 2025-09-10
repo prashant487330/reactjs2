a40ddb513a107dde3ab22b270f05396639ecffa8
prashant patel <prashantpatel487330@gmail.com>
2025-09-10 15:02:51 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
 const App=()=>{
-    
+    const[image,setImage]=useState("")
+    const handleInputImage=(e)=>{
+        setImage(e.target.files[0])
+    }
+    const handleSubmit=async()=>{
+        const formData=new FormData();
+        formData.append("file",image);
+        formData.append("upload_preset","Prashant patel");
+        const response=await axios.post("https://api.cloudinary.com/v1_1/dinqyeur3/image/upload",formData);
+        console.log(response.data);
+        console.log(response.data.url);
+        document.getElementById("myimg").src=response.data.url;
+    }
     return(
         <>
         <h1>Upload Image</h1>
